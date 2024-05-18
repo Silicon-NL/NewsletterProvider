6ed55987fe6ca5259ac95833f9d840ce2ede3138
Nils0009 <143409567+Nils0009@users.noreply.github.com>
2024-05-18 10:33:11 +0200

    Add project files.

---
+++ Infrastructure/Contexts/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Contexts;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionBuilder.UseSqlServer("Server=tcp:blazorwebapp-sqlserver-silicon.database.windows.net,1433;Initial Catalog=BlazorWebApp-DB-Silicon;Persist Security Info=False;User ID=SqlAdmin;Password=Admin123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
+        return new DataContext(optionBuilder.Options);
+    }
+}
---
+++ NewsletterProvider/Functions/Subscribe.cs
@@ -0,0 +1,43 @@
+using Infrastructure.Contexts;
+using Infrastructure.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace NewsletterProvider.Functions
+{
+    public class Subscribe(ILogger<Subscribe> logger, DataContext dataContext)
+    {
+        private readonly ILogger<Subscribe> _logger = logger;
+        private readonly DataContext _dataContext = dataContext;
+
+        [Function("Subscribe")]
+
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+        {
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            if (!string.IsNullOrEmpty(body))
+            {
+                var subscribeEntity = JsonConvert.DeserializeObject<SubscribeEntity>(body);
+                if (subscribeEntity != null)
+                {
+                    var existingSubscriber = await _dataContext.AspNetSubscribers.FirstOrDefaultAsync(x => x.Email == subscribeEntity.Email);
+                    if (existingSubscriber != null)
+                    {
+                        _dataContext.Entry(existingSubscriber).CurrentValues.SetValues(subscribeEntity);
+                        await _dataContext.SaveChangesAsync();
+                        return new OkObjectResult(new { Status = 200, Message = "Subscriber was updated." });
+                    }
+
+                    _dataContext.AspNetSubscribers.Add(subscribeEntity);
+                    await _dataContext.SaveChangesAsync();
+                    return new OkObjectResult(new { Status = 201, Message = "Subscriber is now subscribed." });
+                }
+            }
+            return new BadRequestObjectResult(new { Status = 400, Message = "Unable to subscribe" });
+        }
+    }
+}
